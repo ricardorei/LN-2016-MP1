3793266a0f952331e9011b0dc9dd7cec14e5ef86
RicardoRei <ricardo.rei@ist.utl.pt>
2016-10-11 10:24:16 +0100

    tests added for carvalho and rei words on step 4

--- TestePasso4/test.sh
+++ TestePasso4/test.sh
@@ -1,5 +1,8 @@
-python word2fst.py aleluia > w-aleluia.txt
-fstcompile --isymbols=syms.txt --osymbols=syms.txt w-aleluia.txt | fstarcsort > w-aleluia.fst
+#!/bin/bash
+for i in  aleluia carvalho rei; do
+	python word2fst.py $i > w-$i.txt
+	fstcompile --isymbols=syms.txt --osymbols=syms.txt w-$i.txt | fstarcsort > w-$i.fst
+done
 
 python compact2fst.py 4_vogais.txt > ugly-4_vogais.txt
 fstcompile --isymbols=syms.txt --osymbols=syms.txt ugly-4_vogais.txt | fstarcsort > passo4.fst
@@ -9,3 +12,9 @@ fstcompile --isymbols=syms.txt --osymbols=syms.txt ugly-4_vogais.txt | fstarcsor
 #ALELUI substituicao de VOGAIS (excepto a primeira) por EPS = ALL
 fstcompose w-aleluia.fst passo4.fst > all.fst
 fstdraw --isymbols=syms.txt --osymbols=syms.txt -portrait all.fst | dot -Tpdf > all.pdf
+
+fstcompose w-carvalho.fst passo4.fst > carvalho.fst
+fstdraw --isymbols=syms.txt --osymbols=syms.txt -portrait carvalho.fst | dot -Tpdf > carvalho.pdf
+
+fstcompose w-rei.fst passo4.fst > rei.fst
+fstdraw --isymbols=syms.txt --osymbols=syms.txt -portrait rei.fst | dot -Tpdf > rei.pdf
